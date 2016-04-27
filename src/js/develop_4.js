3c60897db5d255b704683e2d6fbc7f3db1ef1303
Radiksman <radion91@gmail.com>
2016-04-27 14:15:44 +0300

    no message

--- src/js/develop_4.js
+++ src/js/develop_4.js
@@ -1,8 +1,52 @@
+function googleMap(mapWrap){
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(cordX,cordY);
+        var myOptions = {
+            zoom: 14,
+            center: myLatlng,
+            disableDefaultUI: false, //без управляющих елементов
+            mapTypeId: google.maps.MapTypeId.ROADMAP, // SATELLITE - снимки со спутника,
+            zoomControlOptions: {
+               position: google.maps.ControlPosition.LEFT_BOTTOM // позиция слева внизу для упр елементов
+            }
+        }
+        var map = new google.maps.Map(document.getElementById(mapWrap), myOptions);
 
+        var contentString = '<div class="marker-test">'+googleText+'</div>';
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            animation: google.maps.Animation.DROP, // анимация при загрузке карты
+            icon: footerImage //  иконка картинкой
+        });
+
+        /*анимация при клике на маркер*/
+        marker.addListener('click', toggleBounce);
+        function toggleBounce() {
+          if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+          } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+          }
+        }
+        /*/анимация при клике на маркер*/
+
+        /*По клику открываеться инфоблок*/
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+
+    }
+    initialize();
+}
 
 
 $(document).ready(function(){
-
+    googleMap('mapInit'); //id
 });
 
 $(window).load(function(){
